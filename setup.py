ccf477ef459236bbcbbc333fd5c01a79b62a49c5
chuanchenlv <chuanchenlv@tencent.com>
2021-06-19 21:18:43 +0800

    Modify gitignore and add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+VERSION = "test"
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = '<h1 align="center">OpenHowNet</h1>\n'+fh.read().split('### [中文版本](README_ZH.md)\n\n')[1]
+
+setuptools.setup(
+    name="OpenHowNet",
+    version=VERSION,
+    author="THUNLP",
+    author_email="thunlp@gmail.com",
+    description="OpenHowNet",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thunlp/OpenHowNet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
