9c551d9dc9ad6d8cc5b6b444fc5fa0f55fe952b3
chuanchenlv <chuanchenlv@tencent.com>
2021-08-05 19:36:07 +0800

    Modify classes

--- OpenHowNet/Sememe.py
+++ OpenHowNet/Sememe.py
@@ -3,22 +3,21 @@ Sememe Class
 =============
 """
 
+
 class Sememe(object):
     """Sememe class.
     The smallest semantic unit. Described in English and Chinese.
 
     Attributes:
-        en (str): 
-            English word to describe the sememe.
-        ch (str): 
-            Chinese word to describe the sememe.
+        en (str): English word to describe the sememe.
+        ch (str): Chinese word to describe the sememe.
         freq (int): 
             the sememe occurence frequency in HowNet.
         related_sememes_forward (dict): 
             the sememes related with the sememe in HowNet and the sememe is head sememe in triples.
         related_sememes_backward (dict): 
             the sememes related with the sememe in HowNet and the sememe is tail sememe in triples.
-    
+
     Args:
         hownet_sememe (str):
             An sememe in HowNet in the form of (English annotation|Chinese annotation).
@@ -49,11 +48,54 @@ class Sememe(object):
 
         Sememe triple contains (head sememe, relation, tail sememe).
         """
-        self.related_sememes_forward[relation] = tail
+        if relation not in self.related_sememes_forward.keys():
+            self.related_sememes_forward[relation] = []
+        self.related_sememes_forward[relation].append(tail)
 
     def add_related_sememes_backward(self, head, relation, tail):
         """Add a sememe triple to the sememe.
 
         Sememe triple contains (head sememe, relation, tail sememe).
         """
-        self.related_sememes_backward[relation] = head
+        if relation not in self.related_sememes_backward.keys():
+            self.related_sememes_backward[relation] = []
+        self.related_sememes_backward[relation].append(head)
+
+    def get_senses(self):
+        """Get the senses annotated with the sememe.
+
+        Returns:
+            (`list[Sense]`) the list of the senses annotated with the sememe.
+        """
+        return self.senses
+
+    def get_related_sememes(self, return_triples=False):
+        """Get the sememes related with the sememe.
+
+        Args:
+            return_triples (`bool`):
+                You can choose to return the list of triples or return the list of related sememes.
+
+        Returns:
+            (list) the list of triples or return the list of related sememes.
+        """
+        res = []
+        if return_triples:
+            for k, v in self.related_sememes_forward.items():
+                res.extend([(self, k, i) for i in v])
+            for k, v in self.related_sememes_backward.items():
+                res.extend([(i, k, self) for i in v])
+        else:
+            for i in self.related_sememes_forward.values():
+                res.extend(i)
+        return res
+    
+    def get_sememe_via_relation(self, relation, return_triples=False):
+        res = []
+        if relation in self.related_sememes_forward.keys():
+            if return_triples:
+                res.extend([(self, relation, i) for i in self.related_sememes_forward[relation]])
+            else:
+                res.extend(self.related_sememes_forward[relation])
+        return res
+
