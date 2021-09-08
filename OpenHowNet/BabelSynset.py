10f07b9a157255bf35222e0ddf5635f673e64b17
chuanchenlv <chuanchenlv@tencent.com>
2021-09-08 16:42:46 +0800

    Add BabelSynset search functions

---
+++ OpenHowNet/BabelSynset.py
@@ -0,0 +1,44 @@
+"""
+BabelSynset Class
+==================
+"""
+
+from .Download import get_resource
+
+
+class BabelSynset(object):
+    """BabelSynset class.
+
+    Attributes:
+        id (str): The unique identity of the BabelSynset in BabelNet.
+        cat (str): The category of the BabelSynset
+        en_synonyms (list): The English synonyms in the BabelSynset.
+        zh_synonyms (list): The Chinese synonyms in the BabelSynset.
+        en_glosses (list): The English glosses in the BabelSynset.
+        zh_glosses (list): The Chinese glosses in the BabelSynset.
+        related_synsets (dict):
+            The related BabelSynsets and the corresponding relations.
+        sememes (list):
+            The sememes labeled to the BabelSynset.
+    """
+
+    def __init__(self, babel_synset):
+        """Initialize an BabelSynset instance.
+        """
+        self.id = babel_synset['id']
+        self.en_synonyms = babel_synset['en_synonyms']
+        self.zh_synonyms = babel_synset['zh_synonyms']
+        self.en_glosses = babel_synset['en_glosses']
+        self.zh_glosses = babel_synset['zh_glosses']
+        self.sememes = []
+
+    def __repr__(self):
+        """Define how to print the babel synset.
+        """
+        res = self.id + '|' + \
+            self.en_synonyms[0] if len(self.en_synonyms) > 0 else id + '|'
+        res += '|' + self.zh_synonyms[0] if len(self.zh_synonyms) > 0 else ''
+        return res
+
+    def get_sememe_list(self):
+        return self.sememes
