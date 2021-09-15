bc12c832bd3366ced69a50fb63b3e4098e6a6733
chuanchenlv <chuanchenlv@tencent.com>
2021-09-15 17:48:54 +0800

    Add install_requires

--- setup.py
+++ setup.py
@@ -24,5 +24,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'anytree',
+        'setuptools',
+        'tqdm',
+        'requests',
+    ],
     python_requires=">=3.6"
 )
